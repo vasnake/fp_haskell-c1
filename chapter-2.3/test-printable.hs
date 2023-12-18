d4e273cc70fa5a71ecfb050e634f4aea3decefae
Valentin Fedulov <vasnake@gmail.com>
2023-12-18 16:34:40 +0200

    WIP: typeclass

---
+++ chapter-2.3/test-printable.hs
@@ -0,0 +1,14 @@
+module Printable where
+
+class Printable a where
+    toString :: a -> String
+
+instance Printable Bool where
+    toString True = "true"
+    toString _ = "false"
+
+instance Printable () where  
+    toString _ = "unit type"
+
+instance (Printable a, Printable b) => Printable (a, b) where
+    toString (x, y) = "(" ++ toString x ++ "," ++ toString y ++ ")"
